c3a82b0c4c6181bb974e15f6bbb7fcd550b63465
y.izripov <y.izripov@vk.team>
2024-06-30 02:37:51 +0300

    chore: formatter

--- src/hooks/usePan.ts
+++ src/hooks/usePan.ts
@@ -1,9 +1,4 @@
-import {
-    MouseEvent as SyntheticMouseEvent,
-    useCallback,
-    useRef,
-    useState,
-} from 'react';
+import { MouseEvent as SyntheticMouseEvent, useCallback, useRef, useState } from 'react';
 
 type Point = { x: number; y: number };
 // TODO: В статье, которую сенсей мне скидывал, было так, но если делать так, то ts кидает ошибки
@@ -13,7 +8,7 @@ const ORIGIN = { x: 0, y: 0 };
 export default function usePan(): [Point, (e: SyntheticMouseEvent) => void] {
     const [panState, setPanState] = useState<Point>(ORIGIN);
 
-    const lastPointRef = useRef(ORIGIN)
+    const lastPointRef = useRef(ORIGIN);
 
     const pan = useCallback((e: MouseEvent) => {
         const lastPoint = lastPointRef.current;
@@ -23,27 +18,30 @@ export default function usePan(): [Point, (e: SyntheticMouseEvent) => void] {
         setPanState((panState) => {
             const delta = {
                 x: point.x - lastPoint.x,
-                y: point.y - lastPoint.y
-            }
+                y: point.y - lastPoint.y,
+            };
             const offset = {
                 x: panState.x + delta.x,
-                y: panState.y + delta.y
-            }
+                y: panState.y + delta.y,
+            };
 
-            return offset
-        })
-    }, [])
+            return offset;
+        });
+    }, []);
 
     const endPan = useCallback(() => {
         document.removeEventListener('mousemove', pan);
-        document.removeEventListener('mouseup', endPan)
-    }, [pan])
+        document.removeEventListener('mouseup', endPan);
+    }, [pan]);
 
-    const startPan = useCallback((e: SyntheticMouseEvent) => {
-        document.addEventListener('mousemove', pan);
-        document.addEventListener('mouseup', endPan)
-        lastPointRef.current = { x: e.pageX, y: e.pageY }
-    }, [pan, endPan])
+    const startPan = useCallback(
+        (e: SyntheticMouseEvent) => {
+            document.addEventListener('mousemove', pan);
+            document.addEventListener('mouseup', endPan);
+            lastPointRef.current = { x: e.pageX, y: e.pageY };
+        },
+        [pan, endPan],
+    );
 
-    return [panState, startPan]
+    return [panState, startPan];
 }
